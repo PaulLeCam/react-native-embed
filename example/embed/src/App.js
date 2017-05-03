30c6b9dc0abe9f81b7f6d6120f844aed52c2e64b
Paul Le Cam <paul@ulem.net>
2017-05-03 19:42:28 +0100

    Update dependencies

--- example/embed/src/App.js
+++ example/embed/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, View } from 'react-native'
 import Embed from 'react-native-embed'
 
 const styles = StyleSheet.create({
@@ -10,14 +10,28 @@ const styles = StyleSheet.create({
 })
 
 export default class App extends Component {
-  render () {
+  state = {
+    display: false,
+  }
+
+  onPress = () => {
+    this.setState({ display: true })
+  }
+
+  render() {
+    const display = this.state.display
+      ? <Embed
+          ref={e => {
+            this.embed = e
+          }}
+          onEvent={this.onEvent}
+          source={{ uri: 'http://localhost:3001' }}
+        />
+      : <Button onPress={this.onPress} title="Press me" />
+
     return (
       <View style={styles.container}>
-        <Embed
-          ref={e => { this.embed = e }}
-          onEvent={this.onEvent}
-          source={{uri: 'http://localhost:3001'}}
-        />
+        {display}
       </View>
     )
   }
